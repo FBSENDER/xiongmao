d7afc3f64669f5c24d54ec081c29bb86b6213992
fbsender <lmctrello@gmail.com>
2021-02-26 22:09:39 +0800

    update beian

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "jd_media#collection_home"
-  get "collection/:id", to: "jd_media#collection"
-  get "cc/:id", to: "jd_media#collection_category"
-  get "jdsku/:id", to: "jd_media#jdsku"
+  #get "collection/:id", to: "jd_media#collection"
+  #get "cc/:id", to: "jd_media#collection_category"
+  #get "jdsku/:id", to: "jd_media#jdsku"
   get "data", to: "jd_media#data"
 
   #zsh
